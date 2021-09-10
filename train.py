c3dd9ba32a6763f1242243bfdbf8d2d2b5ab8f1a
dell <zimuxiaolin@163.com>
2021-09-10 10:00:19 +0800

    MTL-1

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 
 # Train on CPU (hide GPU) due to memory constraints
 #由于内存限制，使用CPU进行训练
-os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
 #获得数据集的邻接矩阵与特征矩阵
 adj, features = load_data(args.dataset)
@@ -28,6 +28,7 @@ adj_orig = adj
 #sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)->取出邻接矩阵主对角线的元素
 adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
 #移除零元素
+adj_all = adj_orig
 adj_orig.eliminate_zeros()
 
 #将数据集进行划分，test、validation、train、False
@@ -61,6 +62,7 @@ norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.su
 #sp.eye->生成n * n全为1的对角阵
 adj_label = adj_train + sp.eye(adj_train.shape[0])
 adj_label = sparse_to_tuple(adj_label)
+adj_all = sparse_to_tuple(adj_all)
 
 
 #adj_norm -> 行值[a,b,c,d,e]
@@ -83,7 +85,9 @@ adj_label = torch.sparse.FloatTensor(torch.LongTensor(adj_label[0].T),
 features = torch.sparse.FloatTensor(torch.LongTensor(features[0].T), 
                             torch.FloatTensor(features[1]), 
                             torch.Size(features[2]))
-
+adj_all = torch.sparse.FloatTensor(torch.LongTensor(adj_all[0].T),
+                            torch.FloatTensor(adj_all[1]),
+                            torch.Size(adj_all[2]))
 #to_dense()->转化为稠密矩阵形式
 #.view()将张量转化为一维向量形式
 #=1的位置为True，做一张Mask tensor
@@ -105,7 +109,7 @@ model = getattr(model,args.model)(adj_norm)
 model = model.cuda()
 #使用Adam优化器，参数为model.parammeters(),learning_rate
 optimizer = Adam(model.parameters(), lr=args.learning_rate)
-scheduler1 = lr_scheduler.StepLR(optimizer,step_size=200000,gamma=0.8)
+scheduler1 = lr_scheduler.StepLR(optimizer,step_size=20000,gamma=0.95)
 
 #validation_edges and validation_edges_false vs A_pred
 def get_scores(edges_pos, edges_neg, adj_rec):
@@ -154,7 +158,11 @@ def get_acc(adj_rec, adj_label):
     #adj_rec->预测矩阵；adj_label-> (A + I)
     #变为1维
     #为何用long
-    labels_all = adj_label.to_dense().view(-1).long()
+    try:
+        labels_all = adj_label.to_dense().view(-1).long()
+    except AttributeError:
+        print('no to_dense() attribute')
+        labels_all = adj_label.view(-1).long()
     #预测矩阵大于0.5的边置为1，小于0.5为0
     preds_all = (adj_rec > 0.5).view(-1).long()
     #对应为值相等的数量之和/所有的边数
@@ -203,13 +211,24 @@ Loss = Loss.cuda()
 features = CPU_data_normal_2d(features.to_dense())
 features = features.cuda()
 adj_label = adj_label.cuda()
+adj_all = adj_all.cuda()
 acc_history = []
 roc_history = []
 ap_history = []
+all_acc_history = []
 # train model
 #开始训练
 
-def MTL(loss, Floss):
+#use this method the grad in different loss dnt added
+grads = {}
+MTLoss_history = []
+
+def save_grad(name):
+    def hook(grad):
+        grads[name] = grad
+    return hook
+
+def MTL(loss, Floss, MTLoss):
     '''
     使用多任务学习的多个梯度来决定最终梯度
     :param loss: 带噪声的损失
@@ -217,39 +236,62 @@ def MTL(loss, Floss):
     :return:
     '''
     #对loss进行反向传播，获取各层的梯度
+    print('loss:', loss)
+    print('Floss:', Floss)
+    A_pred.register_hook(save_grad('z'))
     loss.backward(retain_graph = True)
-    out_loss_grad = model.gcn_out.weight.grad
-    mean_loss_grad = model.gcn_mean.weight.grad
-    base_loss_grad = model.base_gcn.weight.grad
+    '''#if not use the clone, out_loss_grad is detached with the grad's memory
+    out_loss_grad = model.gcn_out.weight.grad.clone()
+    mean_loss_grad = model.gcn_mean.weight.grad.clone()
+    base_loss_grad = model.base_gcn.weight.grad.clone()
     #将计算图中的梯度清零，准备对第二种loss进行反向传播
-    optimizer.zero_grad()
+    #optimizer.zero_grad()
     Floss.backward()
+    print(grads['z'])
     out_Floss_grad = model.gcn_out.weight.grad
     mean_Floss_grad = model.gcn_mean.weight.grad
     base_Floss_grad = model.base_gcn.weight.grad
     #使用gcn_out层的梯度进行比率计算
     #solution1：将矩阵按列进行切分，分为多个列向量，列向量之间求得帕累托最优，计算比例
     theta1 = out_loss_grad
-    theta2 = out_Floss_grad
+    theta2 = out_Floss_grad'''
+    theta1 = grads['z'].view(-1)
+    optimizer.zero_grad()
+    Floss.backward(retain_graph = True)
+    theta2 = grads['z'].view(-1)
+    theta1 = theta1.reshape(1,7333264)
+    theta2 = theta2.reshape(1,7333264)
     #列向量之间做差
-    part1 = torch.mm((theta2 - theta1).T, theta2)
+    part1 = torch.mm((theta2 - theta1), theta2.T)
     #取主对角线元素
-    part1 = torch.diagonal(part1)
-    part2 = torch.norm(theta1 - theta2, p = 2, dim = 0)
+    #part1 = torch.diagonal(part1)
+    #part2 = torch.norm(theta1 - theta2, p = 2, dim = 0)
+    part2 = torch.norm(theta1 - theta2, p = 2)
     #二范数的平方
-    part2.pow(2)
+    part2 = part2.pow(2)
     alpha = torch.div(part1, part2)
     min = torch.ones_like(alpha)
     alpha = torch.where(alpha > 1, min, alpha)
     min = torch.zeros_like(alpha)
     alpha = torch.where(alpha < 0, min, alpha)
+    alpha1 = alpha
+    alpha2 = 1 - alpha
+    print('alpha1:', alpha1)
+    optimizer.zero_grad()
+    MTLoss = alpha1 * loss + alpha2 * Floss
+    MTLoss_history.append(MTLoss.item())
+    MTLoss.backward()
     #alpha theta1 & (1 - alpha) theta2
     #将alpha等维度拓展
-    alpha1 = alpha.repeat(theta1.shape[0], 1)
-    alpha2 = (1 - alpha).repeat(theta1.shape[0], 1)
-    model.gcn_out.weight.grad = torch.mul(alpha1, out_loss_grad) + torch.mul(alpha2, out_Floss_grad)
-    model.gcn_mean.weight.grad = torch.mul(alpha1, mean_loss_grad) + torch.mul(alpha2, mean_Floss_grad)
-    model.base_gcn.weight.grad = torch.mul(alpha1, base_loss_grad) + torch.mul(alpha2, base_Floss_grad)
+    #alpha1 = alpha.repeat(theta1.shape[0], 1)
+    #alpha2 = 1 - alpha1
+    #a = torch.mul(alpha1, out_loss_grad)
+    #b = torch.mul(alpha2, out_Floss_grad)
+
+    #model.gcn_out.weight.grad = torch.mul(alpha1, out_loss_grad) + torch.mul(alpha2, out_Floss_grad)
+
+    #model.gcn_mean.weight.grad = torch.mul(alpha1, mean_loss_grad) + torch.mul(alpha2, mean_Floss_grad)
+    #model.base_gcn.weight.grad = torch.mul(alpha1, base_loss_grad) + torch.mul(alpha2, base_Floss_grad)
 
 
 for epoch in range(args.num_epoch):
@@ -276,14 +318,17 @@ for epoch in range(args.num_epoch):
         loss -= kl_divergence
     #误差反向传播
     #loss.backward()
-    MTL(loss, Floss)
+    MTLoss = Floss
+    MTL(loss, Floss, MTLoss)
     #梯度下降
     optimizer.step()
     print('epoch %d learning rate: %f' % (epoch, optimizer.param_groups[0]['lr']))
     scheduler1.step()
     #计算ACC
     train_acc = get_acc(A_pred,adj_label)
+    all_acc = get_acc(A_pred,adj_all)
     acc_history.append(train_acc.item())
+    all_acc_history.append(all_acc.item())
     #validation_edges and validation_edges_false vs A_pred
     #在训练过程中使用validation; validation的主要作用是来验证是否过拟合、以及用来调节训练参数等
     #边训练边看到训练的结果，及时判断学习状态
@@ -291,20 +336,18 @@ for epoch in range(args.num_epoch):
     val_roc, val_ap = get_scores(val_edges, val_edges_false, A_pred)
     roc_history.append(val_roc.item())
     ap_history.append(val_ap.item())
-    print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(loss.item()),
+    print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(MTLoss.item()),
           "train_acc=", "{:.5f}".format(train_acc), "val_roc=", "{:.5f}".format(val_roc),
-          "val_ap=", "{:.5f}".format(val_ap),
+          "val_ap=", "{:.5f}".format(val_ap),"adj_all_acc=", "{:.5f}".format(all_acc),
           "time=", "{:.5f}".format(time.time() - t))
 
 
 test_roc, test_ap = get_scores(test_edges, test_edges_false, A_pred)
+print('Loss_&_ACC_H3_MTL_400w')
 print("End of training!", "test_roc=", "{:.5f}".format(test_roc),
       "test_ap=", "{:.5f}".format(test_ap))
 
-
-
-
-def plot_loss_with_acc(loss_history,Floss_history,Loss_history,acc_history,roc_history,ap_history):
+def plot_loss_with_acc(loss_history,Floss_history,Loss_history,acc_history,roc_history,ap_history,all_acc_history):
     fig = plt.figure(figsize=(18, 10))
     ax1 = fig.add_subplot(121)
     plot1 = plt.plot(range(len(loss_history)),loss_history,c = 'b',label = 'AP_loss')
@@ -318,13 +361,15 @@ def plot_loss_with_acc(loss_history,Floss_history,Loss_history,acc_history,roc_h
     plot4 = plt.plot(range(len(acc_history)),acc_history,c = 'y',label = 'ACC')
     plot5 = plt.plot(range(len(roc_history)),roc_history,c = 'b',label = 'ROC')
     plot6 = plt.plot(range(len(ap_history)),ap_history,c = 'r',label = 'AP')
+    plot7 = plt.plot(range(len(all_acc_history)),all_acc_history,c = 'g',label = 'ALL_ACC')
     ax2.set_title('ACC')
     ax2.set_xlabel('epoch')
     ax2.set_ylabel('percent')
-    ax2.legend(fontsize = 'large', loc = 'upper right')
-    plt.savefig('Loss_&_ACC_H3_025_975_1_0_400w.png')
+    ax2.legend(fontsize = 'large', loc = 'lower right')
+    #plt.savefig('Loss_&_ACC_H3_025_975_975_025_400w.png')
+    plt.savefig('Loss_&_ACC_H3_MTL_400w.png')
 
-plot_loss_with_acc(soft_max_Loss.loss_history,soft_max_Loss.Floss_history,soft_max_Loss.Loss_history,acc_history,roc_history,ap_history)
+plot_loss_with_acc(soft_max_Loss.loss_history,soft_max_Loss.Floss_history,MTLoss_history,acc_history,roc_history,ap_history,all_acc_history)
 
 '''print(model.Z)
 Z = model.Z
